c378632f3c0e73287d05ac2a75c6be6e3b68d779
Scott Munday <scottmunday84@gmail.com>
2026-08-06 21:41:41 -0400

    Updated entity/system interaction so they work much more cleaner and reliable

--- moonhowl-ecs/src/entity.rs
+++ moonhowl-ecs/src/entity.rs
@@ -1,18 +1,19 @@
 use crate::component::IComponent;
 use crate::system::System;
 use std::any::TypeId;
+use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 
 pub struct Entity {
     components: HashMap<TypeId, Box<dyn IComponent>>,
-    registered_components: HashMap<TypeId, HashSet<usize>>,
+    read_by: RefCell<HashMap<TypeId, HashSet<usize>>>,
 }
 
 impl Entity {
     pub fn new() -> Self {
         Self {
             components: HashMap::new(),
-            registered_components: HashMap::new(),
+            read_by: RefCell::new(HashMap::new()),
         }
     }
 
@@ -20,48 +21,50 @@ impl Entity {
         self.components.contains_key(&TypeId::of::<T>())
     }
 
-    pub fn has_registered_component<T: IComponent>(&self, id: usize) -> bool {
-        if !self.has_component::<T>() {
-            return false;
-        }
-
-        if let Some(registered_component) = self.registered_components.get(&TypeId::of::<T>()) {
-            return !registered_component.contains(id);
-        }
-
-        true
+    pub fn is_component_read<T: IComponent>(&self, system_id: usize) -> bool {
+        self.read_by
+            .borrow()
+            .get(&TypeId::of::<T>())
+            .is_some_and(|readers| readers.contains(&system_id))
     }
 
-    pub fn get_component<T: IComponent>(&self) -> Option<&T> {
+    pub fn has_read_component<T: IComponent>(&self, system_id: usize) -> bool {
+        self.has_component::<T>() && self.is_component_read::<T>(system_id)
+    }
+
+    pub fn has_unread_component<T: IComponent>(&self, system_id: usize) -> bool {
+        self.has_component::<T>() && !self.is_component_read::<T>(system_id)
+    }
+
+    pub fn get_component<T: IComponent>(&self, system_id: usize) -> Option<&T> {
+        if !self.has_component::<T>() {
+            return None;
+        }
+
+        self.read_by
+            .borrow_mut()
+            .entry(TypeId::of::<T>())
+            .or_default()
+            .insert(system_id);
+
         self.components
             .get(&TypeId::of::<T>())
             .and_then(|component| (**component).as_any().downcast_ref::<T>())
     }
 
-    pub fn get_registered_component<T: IComponent>(&mut self, id: usize) -> Option<&T> {
-        if !self.has_component::<T>() {
-            return None;
-        }
-
-        if let Some(registered_component) = self.registered_components.get_mut(&TypeId::of::<T>()) {
-            registered_component.insert(id);
-        };
-
-        self.get_component::<T>()
-    }
-
-    pub fn push_component<T: IComponent>(&mut self, component: T) -> &mut Self {
+    pub fn insert_component<T: IComponent>(&mut self, component: T) -> &mut Self {
         let boxed_component = Box::new(component);
         self.components.insert(TypeId::of::<T>(), boxed_component);
-        self.registered_components
+        self.read_by
+            .get_mut()
             .insert(TypeId::of::<T>(), HashSet::new());
 
         self
     }
 
-    pub fn pull_component<T: IComponent>(&mut self) -> &mut Self {
+    pub fn remove_component<T: IComponent>(&mut self) -> &mut Self {
         self.components.remove(&TypeId::of::<T>());
-        self.registered_components.remove(&TypeId::of::<T>());
+        self.read_by.get_mut().remove(&TypeId::of::<T>());
 
         self
     }
@@ -77,32 +80,31 @@ impl Entity {
     }
 }
 
-pub enum EntitySystem<'a> {
-    Reader(&'a Entity, &'a System),
-    Writer(&'a mut Entity, &'a System),
-}
+pub struct EntitySystem<'a>(&'a Entity, &'a System);
 
 impl<'a> EntitySystem<'a> {
-    pub fn new_reader(entity: &'a Entity, system: &'a System) -> Self {
-        Self::Reader(entity, system)
-    }
-
-    pub fn new_writer(entity: &'a mut Entity, system: &'a System) -> Self {
-        Self::Writer(entity, system)
+    pub fn new(entity: &'a Entity, system: &'a System) -> Self {
+        Self(entity, system)
     }
 
     pub fn has_component<T: IComponent>(&self) -> bool {
-        match self {
-            EntitySystem::Reader(entity, system) => system.has_component::<T>(entity),
-            EntitySystem::Writer(entity, system) => system.has_component::<T>(entity),
-        }
+        self.0.has_component::<T>()
     }
 
-    pub fn get_component<T: IComponent>(&mut self) -> Option<&T> {
-        match self {
-            EntitySystem::Reader(entity, system) => entity.get_component::<T>(),
-            EntitySystem::Writer(entity, system) => system.get_component::<T>(entity),
-        }
+    pub fn is_component_read<T: IComponent>(&self) -> bool {
+        self.0.is_component_read::<T>(self.1.get_id())
+    }
+
+    pub fn has_read_component<T: IComponent>(&self) -> bool {
+        self.0.has_read_component::<T>(self.1.get_id())
+    }
+
+    pub fn has_unread_component<T: IComponent>(&self) -> bool {
+        self.0.has_unread_component::<T>(self.1.get_id())
+    }
+
+    pub fn get_component<T: IComponent>(&self) -> Option<&T> {
+        self.0.get_component::<T>(self.1.get_id())
     }
 
     pub fn check<F>(&self, predicate: F) -> EntityCheck
--- moonhowl-ecs/src/system.rs
+++ moonhowl-ecs/src/system.rs
@@ -19,10 +19,22 @@ impl System {
     }
 
     pub fn has_component<T: IComponent>(&self, entity: &Entity) -> bool {
-        entity.has_registered_component::<T>(self.0)
+        entity.has_component::<T>()
     }
 
-    pub fn get_component<'a, T: IComponent>(&self, entity: &'a mut Entity) -> Option<&'a T> {
-        entity.get_registered_component::<T>(self.0)
+    pub fn is_component_read<T: IComponent>(&self, entity: &Entity) -> bool {
+        entity.is_component_read::<T>(self.0)
+    }
+
+    pub fn has_read_component<T: IComponent>(&self, entity: &Entity) -> bool {
+        entity.has_read_component::<T>(self.0)
+    }
+
+    pub fn has_unread_component<T: IComponent>(&self, entity: &Entity) -> bool {
+        entity.has_unread_component::<T>(self.0)
+    }
+
+    pub fn get_component<'a, T: IComponent>(&self, entity: &'a Entity) -> Option<&'a T> {
+        entity.get_component::<T>(self.0)
     }
 }
