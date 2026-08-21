17a4e670df79c09faf32cbd8c763b0a879ce71c9
Scott Munday <scottmunday84@gmail.com>
2026-08-21 16:28:38 -0400

    Decided to keep with what I had

--- ecs/src/sparse_set.rs
+++ ecs/src/sparse_set.rs
@@ -70,8 +70,6 @@ impl<T> SparseSet<T> {
         &mut self.dense
     }
 
-    /// `entities()` and `values_mut()` at once, index-aligned -- a single call so both
-    /// borrows can be held together (two separate calls can't, since one needs `&mut self`).
     pub fn entities_and_values_mut(&mut self) -> (&[Entity], &mut [T]) {
         (&self.dense_entities, &mut self.dense)
     }
